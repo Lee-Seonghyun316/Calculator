42daae5f092d99bab2561d5680cd6baafe8237de
Lee-Seonghyun316 <mn0316@naver.com>
2020-12-24 15:42:30 +0900

    [fix] 입력 오류 수정

--- Day1/js/index.js
+++ Day1/js/index.js
@@ -1,5 +1,4 @@
-var all = "";
-
+var all = ""; //모든함수에서쓸수있는전역변수
 function add(num) {
   all = all + num;
   document.getElementById("display").value = all;
@@ -26,26 +25,3 @@ function abs(all) {
 function pow(all) {
   all.value = Math.pow(all.value, 2);
 }
-// // import App from './components/App.js';
-// //
-// // App();
-//
-// function add(char) {
-//   var exp = document.getElementById("exp");
-//   exp.value = exp.value + char;
-// }
-// function calculate() {
-//   var exp = document.getElementById("exp");
-//   var result = eval(exp.value);
-//   document.getElementById("result").value = result;
-// }
-// function reset() {
-//   document.getElementById("display").value = "";
-//   document.getElementById("result").value = "";
-// }
-//
-// // var all = "";
-// //
-// // function tan(all) {
-// //   all.value = Math.tan((all.value * 3.141592) / 180);
-// // }
